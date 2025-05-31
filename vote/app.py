707d84f79618057843d30f191c08b7dc1b604b0f
vovinh <vovinhss@gmail.com>
2025-05-31 01:36:47 +0000

    minor update

--- vote/app.py
+++ vote/app.py
@@ -16,10 +16,12 @@ gunicorn_error_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
 
+
 def get_redis():
     if not hasattr(g, "redis"):
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
+
 
 @app.route("/", methods=["POST", "GET"])
 def hello():
